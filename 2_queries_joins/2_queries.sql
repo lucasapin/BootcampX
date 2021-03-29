a056862c82463c33d0d55de13ad427032a19d547
Lucas Pinheiro <lucaspinheiru@hotmail.com>
2021-03-29 19:51:46 +0000

    2_queries finished

---
+++ 2_queries_joins/2_queries.sql
@@ -0,0 +1,13 @@
+/*Query #1*/
+SELECT SUM(assignment_submissions.duration) as total_duration
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+WHERE students.name = 'Ibrahim Schimmel';
+
+/*Query #2*/
+SELECT SUM(assignment_submissions.duration) as total_duration
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = 'FEB12';
+
