8ea86cd960b4bb3e03cebd0b4ec46b8049856fa6
Lucas Pinheiro <lucaspinheiru@hotmail.com>
2021-03-31 22:58:33 +0000

    Query injection risk annulled

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,17 +7,24 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryString1 = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teachers.name;
-`)
+WHERE cohorts.name = $1
+ORDER BY teachers.name
+LIMIT $2;
+`;
+
+const cohortsName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`${cohortsName}`, limit];
+
+pool.query(queryString1, values)
   .then(res => {
     res.rows.forEach(row => {
-      console.log(`${row.cohort}: ${row.teacher}`)
-    })
-  });
+      console.log(`${row.cohort}: ${row.teacher}`);
+    });
+  }).catch(err => console.error('query error', err.stack));
